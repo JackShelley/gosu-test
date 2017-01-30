16b44b4dcc43876611caae52bcc98704e7759c7d
Jack Shelley <bearliver@gmail.com>
2017-01-29 18:25:46 -0800

    text wrapping functional

--- lib/text.rb
+++ lib/text.rb
@@ -2,29 +2,76 @@ require 'gosu'
 
 class TextBox < Gosu::Image
 
-  def initialize(window, text)
+  def initialize(window, speaker, text, xpos, ypos)
     @original_text = text
-    @text = text
+    @speaker = speaker
+    @xpos = xpos
+    @ypos = ypos
+    @original_text = text
     @window = window
-
     @image = Gosu::Image.new(window, "assets/textbox.png", false)
     @medium_font = Gosu::Font.new(@window, "Alagard", 20)
+    @large_font = Gosu::Font.new(@window, "Alagard", 25)
     @output = []
+    @counter = 0
+    @showing = true
+  end
+
+  def reformat_wrapped(s, width=45)
+    lines = []
+    line = ""
+    s.split(/\s+/).each do |word|
+      if line.size + word.size >= width
+        lines << line
+        line = word
+      elsif line.empty?
+       line = word
+      else
+       line << " " << word
+     end
+     end
+     lines << line if line
+    return lines
+  end
+
+  def showing?
+    @showing
+  end
+
+  def show
+    @showing = true
+  end
+
+  def hide
+    @showing = false
   end
 
   def update_text
-    current_last_letter = @text.length-1
-    @output <<  @original_text[current_last_letter]
-    @text = @output.join("")
+    @counter += 1
+    @text1 = @original_text[0..@counter][0...45]
+    if @counter > 45 
+      @text2 = @original_text[0..@counter][45..@counter][0...45]
+      if @counter > 90 
+        @text3 = @original_text[0..@counter][90..@counter][0...45]
+      end
+    end
+  end
+
+  def draw_name
+    draw_text(95, 355, "#{@speaker}", @large_font, Gosu::Color::BLACK )
   end
 
   def draw 
     pos_x = -20
     pos_y = 300
-    # @image = @action[Gosu.milliseconds / 140 % @action.size]
-    # @frame = Gosu.milliseconds / 140 % @action.size
-    @image.draw(pos_x, pos_y, 1, 6, 6)
-    draw_text(pos_x+200, pos_y+90, "#{@text}", @medium_font, 0xff_ffffffd1000)
-
+    if showing?
+      draw_name
+      # @image = @action[Gosu.milliseconds / 140 % @action.size]
+      # @frame = Gosu.milliseconds / 140 % @action.size
+      @image.draw(pos_x, pos_y, 1, 6, 6)
+      draw_text(95, 380, "#{@text1}", @medium_font, Gosu::Color::BLACK )
+      draw_text(95, 400, "#{@text2}", @medium_font, Gosu::Color::BLACK )
+      draw_text(95, 420, "#{@text3}", @medium_font, Gosu::Color::BLACK )
+    end
   end
 end
